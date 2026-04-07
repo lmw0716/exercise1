a4a29bc146e214a9053ace2ed9db070ca32ea20d
Min Woo Lee <lmw071632@gmail.com>
2026-04-07 13:52:33 +0900

    Implement doubly linked list

--- mydlist.hpp
+++ mydlist.hpp
@@ -28,43 +28,89 @@ private:
 public:
 	MyDoublyLinkedList()
 	{
+		head = new MyNode<T>();
+		tail = new MyNode<T>();
+		head->setNext(tail);
+		head->setPrev(nullptr);
+		tail->setPrev(head);
+		tail->setNext(nullptr);
+		isize = 0;
 	}
 
 	~MyDoublyLinkedList()
 	{
+		MyNode<T>* cur = head->getNext();
+		while (cur != tail)
+		{
+			MyNode<T>* next = cur->getNext();
+			delete cur;
+			cur = next;
+		}
+		delete head;
+		delete tail;
 	}
 
 	bool isEmpty()
 	{
+		return isize == 0;
 	}
 
 	int getSize()
 	{
+		return isize;
 	}
 
 
 	MyNode<T>* getHeadNode()
 	{
+		return head->getNext();
 	}
 
 	MyNode<T>* getTailNode()
 	{
+		return tail->getPrev();
 	}
 
 	void addToHead(T* in)
 	{
+		MyNode<T>* node = new MyNode<T>();
+		node->set(in);
+		node->setNext(head->getNext());
+		node->setPrev(head);
+		head->getNext()->setPrev(node);
+		head->setNext(node);
+		isize++;
 	}
 
 	void removeFromHead()
 	{
+		if (isEmpty()) return;
+		MyNode<T>* node = head->getNext();
+		head->setNext(node->getNext());
+		node->getNext()->setPrev(head);
+		delete node;
+		isize--;
 	}
 
 
 	void addToTail(T* in)
 	{
+		MyNode<T>* node = new MyNode<T>();
+		node->set(in);
+		node->setPrev(tail->getPrev());
+		node->setNext(tail);
+		tail->getPrev()->setNext(node);
+		tail->setPrev(node);
+		isize++;
 	}
 
 	void removeFromTail()
 	{
+		if (isEmpty()) return;
+		MyNode<T>* node = tail->getPrev();
+		tail->setPrev(node->getPrev());
+		node->getPrev()->setNext(tail);
+		delete node;
+		isize--;
 	}
 };
